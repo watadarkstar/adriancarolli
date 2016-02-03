72a2a52b01c5782e60ee93c933f87a30f863d34d
Adrian Carolli <adrian.carolli1@gmail.com>
2016-02-03 11:16:16 -0500

    deploy

---
+++ assets/js/main.js
@@ -0,0 +1,195 @@
+
+
+$(function () {
+  var waypoint = new Waypoint({
+    element: document.getElementById('chart-waypoint'),
+    handler: function(direction) {
+        $('.chart').easyPieChart({
+          barColor: '#feb312',
+          scaleColor: false,
+          lineWidth: 5
+        });
+    },
+    offset: 'bottom-in-view',
+    triggerOnce: true,
+  });
+
+  $('#p1-sparkgig').magnificPopup({
+    items: [
+      {src: 'assets/img/portfolio/sparkgig-work1.png'},
+      {src: 'assets/img/portfolio/sparkgig-work2.png'},
+      {src: 'assets/img/portfolio/sparkgig-work3.png'}
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+  });
+
+  $('#p2-math').magnificPopup({
+    items: [
+      {src: 'assets/img/portfolio/math-work1.png'},
+      {src: 'assets/img/portfolio/math-work2.png'},
+      {src: 'assets/img/portfolio/math-work3.png'},
+      {src: 'assets/img/portfolio/math-work4.png'},
+      {src: 'assets/img/portfolio/math-work5.png'},
+      {src: 'assets/img/portfolio/math-work6.png'}
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+  });
+
+  $('#p2-visibli').magnificPopup({
+    items: [
+      {src: 'assets/img/portfolio/visibli-work1.png'},
+      {src: 'assets/img/portfolio/visibli-work2.png'},
+      {src: 'assets/img/portfolio/visibli-work3.png'}
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+  });
+
+  $("#chev-header").click(function(e){
+    $('html, body').animate({
+        scrollTop: $("#section2").offset().top
+    }, 500);
+  });
+
+});
+
+/* DESKTOP */
+
+function pJS_desktop(){
+  particlesJS('particles-js', {
+    particles: {
+      color: '#fff',
+      shape: 'circle',
+      opacity: 1,
+      size: 2.5,
+      size_random: true,
+      nb: 150,
+      line_linked: {
+        enable_auto: true,
+        distance: 250,
+        color: '#fff',
+        opacity: 0.5,
+        width: 1,
+        condensed_mode: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 600
+        }
+      },
+      anim: {
+        enable: true,
+        speed: 3
+      }
+    },
+    interactivity: {
+      enable: true,
+      mouse: {
+        distance: 200
+      },
+      detect_on: 'canvas',
+      mode: 'grab',
+      line_linked: {
+        opacity: .5
+      },
+      events: {
+        onclick: {
+          enable: true,
+          mode: 'push',
+          nb: 4
+        }
+      }
+    },
+    retina_detect: true
+  });
+}
+
+
+
+/* MOBILE / TABLET */
+
+function pJS_mobile(){
+  particlesJS('particles-js', {
+    particles: {
+      color: '#fff',
+      shape: 'circle',
+      opacity: 1,
+      size: 2.5,
+      size_random: true,
+      nb: 40,
+      line_linked: {
+        enable_auto: false,
+        distance: 250,
+        color: '#fff',
+        opacity: 0.5,
+        width: 1,
+        condensed_mode: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 600
+        }
+      },
+      anim: {
+        enable: true,
+        speed: 3
+      }
+    },
+    interactivity: {
+      enable: false,
+      mouse: {
+        distance: 200
+      },
+      detect_on: 'canvas',
+      mode: 'grab',
+      line_linked: {
+        opacity: .5
+      },
+      events: {
+        onclick: {
+          enable: true,
+          mode: 'push',
+          nb: 4
+        }
+      }
+    },
+    retina_detect: true
+  });
+}
+
+
+/* LAUNCH */
+
+if(window.innerWidth > 1100){
+  pJS_desktop();
+}else{
+  pJS_mobile();
+}
+
+/* on resize */
+
+window.addEventListener('resize', function() {
+  checkOnResize();
+}, true);
+
+function checkOnResize(){
+  if(window.innerWidth > 1100){
+    if(pJS.particles.nb != 150){
+      console.log('desktop mode')
+      pJS.fn.vendors.destroy();
+      pJS_desktop();
+    }
+  }else{
+    if(pJS.particles.nb == 150){
+      console.log('mobile mode');
+      pJS.fn.vendors.destroy();
+      pJS_mobile();
+    }
+  }
+}
