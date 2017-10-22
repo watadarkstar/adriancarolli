de1a19cb836b231524415f897330747b6f77b05e
Adrian Carolli <adrian.carolli1@gmail.com>
2017-10-22 19:38:16 -0400

    added airbnb linter - fixed lint errors

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import bgImg from '../../assets/me.jpg';
 import bgPatternImg from '../../assets/bgPattern.png';
-import Particles from 'react-particles-js';
+import Nav from '../shared/Nav';
 
 const Header = () => {
-  const { containerStyles, innerContainerStyles, boxStyles, h1Styles } = styles;
-  
+  const {
+    containerStyles, innerContainerStyles, boxStyles, h1Styles,
+  } = styles;
+
   return (
     <div style={containerStyles}>
+      <Nav />
       <div>
-        <Particles style={innerContainerStyles}/>
+        <Particles style={innerContainerStyles} />
         <div style={boxStyles}>
-            <h1 style={h1Styles}>I'm Adrian Carolli</h1>
-            <h2>Beautifully crafting web and mobile experiences</h2>
+          <h1 style={h1Styles}>{'I\'m Adrian Carolli'}</h1>
+          <h2>Beautifully crafting web and mobile experiences</h2>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 const styles = {
   containerStyles: {
     backgroundColor: 'red',
     backgroundImage: `url(${bgImg})`,
     backgroundSize: 'cover',
-    backgroundPosition: 'center'
+    backgroundPosition: 'center',
   },
   innerContainerStyles: {
     backgroundColor: '#1b1f29',
@@ -46,12 +50,12 @@ const styles = {
     textAlign: 'center',
     zIndex: 100,
     backgroundColor: 'rgba(0,0,0,.2)',
-    opacity: 1
+    opacity: 1,
   },
   h1Styles: {
     fontSize: 100,
-    color: 'white'
-  }
+    color: 'white',
+  },
 };
 
 export default Header;
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Header from './Header';
+
+const Home = () => <Header />;
+
+export default Home;
