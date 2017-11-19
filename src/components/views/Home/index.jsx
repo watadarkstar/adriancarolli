78213401dabee79c96a6411bab0672270974a70b
Adrian Carolli <adrian.carolli1@gmail.com>
2017-11-19 16:33:45 -0500

    Implements the change of the active nav item on scroll

--- src/components/views/Home/index.jsx
+++ src/components/views/Home/index.jsx
@@ -10,37 +10,58 @@ import FixedMenu from '../../common/FixedMenu';
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { visible: false, activeItem: "Home" };
   }
 
   showFixedMenu() { this.setState({ visible: true }); }
   hideFixedMenu() { this.setState({ visible: false }); }
 
   render() {
-    const { visible } = this.state;
+    const { visible, activeItem } = this.state;
 
     return (
       <div style={{ height: '100%' }}>
-        { visible ? <FixedMenu activeItem="Home" /> : null }
+        { visible ? <FixedMenu activeItem={activeItem} /> : null }
         <Visibility
           id="Home"
           onBottomPassed={() => this.showFixedMenu()}
           onBottomVisible={() => this.hideFixedMenu()}
+          onTopPassed={() => this.setState({ activeItem: 'Home' })}
           once={false}
           style={{ height: '100%' }}
         >
           <Splash />
         </Visibility>
-        <Visibility id="About">
+        <Visibility
+          id="About"
+          onTopPassedReverse={() => this.setState({ activeItem: 'Home' })}
+          onTopPassed={() => this.setState({ activeItem: 'About' })}
+          once={false}
+        >
           <About />
         </Visibility>
-        <Visibility id="Skills">
+        <Visibility
+          id="Skills"
+          onTopPassedReverse={() => this.setState({ activeItem: 'About' })}
+          onTopPassed={() => this.setState({ activeItem: 'Skills' })}
+          once={false}
+        >
           <Skills />
         </Visibility>
-        <Visibility id="Portfolio">
+        <Visibility
+          id="Portfolio"
+          onTopPassedReverse={() => this.setState({ activeItem: 'Skills' })}
+          onTopPassed={() => this.setState({ activeItem: 'Portfolio' })}
+          once={false}
+        >
           <Portfolio />
         </Visibility>
-        <Visibility id="Contact">
+        <Visibility
+          id="Contact"
+          onTopPassedReverse={() => this.setState({ activeItem: 'Portfolio' })}
+          onTopPassed={() => this.setState({ activeItem: 'Contact' })}
+          once={false}
+        >
           <Contact />
         </Visibility>
       </div>
