5840f8f713879cc8322ffb66c319412489f48511
Adrian Carolli <adrian.carolli1@gmail.com>
2017-11-21 17:20:58 -0500

    Google Analytics
    - ScrollToTop component is now RouteChange

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Switch } from 'react-router';
 import { Route, BrowserRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import Home from './components/views/Home';
 import PortfolioPage from './components/views/PortfolioPage';
 import NotFound from './components/views/NotFound';
 import data from './store/portfolio/data';
 
+ReactGA.initialize('UA-28934209-5');
+
 const Router = () => (
   <div style={{ height: '100%' }}>
     <BrowserRouter>
       <div style={{ height: '100%' }}>
-        <Route path="/" component={ScrollToTop} />
+        <Route path="/" component={RouteChange} />
         <Switch>
           <Route exact path="/" render={() => <Home />} />
           <Route exact path="/portfolio/penta" render={() => <PortfolioPage data={data.penta} />} />
@@ -28,7 +31,8 @@ const Router = () => (
   </div>
 );
 
-const ScrollToTop = () => {
+const RouteChange = () => {
+  ReactGA.pageview(window.location.pathname + window.location.search);
   window.scrollTo(0, 0);
   return null;
 };
