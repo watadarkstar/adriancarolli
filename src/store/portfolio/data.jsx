268ac9de822183189ba86ef07af652d45f0c3b2d
Adrian Carolli <adrian.carolli1@gmail.com>
2017-11-22 15:56:44 -0500

    Updates descriptions of portfolio items

--- src/store/portfolio/data.jsx
+++ src/store/portfolio/data.jsx
@@ -12,7 +12,7 @@ export default {
     image1: pentaimage1,
     image2: pentaimage2,
     name: 'Penta Medical',
-    description: 'Description coming soon...',
+    description: 'Penta Medical is developing a wearable device that makes the treatment of soft tissue injuries more convenient. My role was to lead the development of their Android and iOS app, which connects to the wearable device via Bluetooth. In three months, I successfully launched the app on the Android and iOS store.',
     link: '/portfolio/penta',
     website: 'http://pentamedical.ca/',
     roles: ['Lead Software Engineer', 'React Native', 'Product Management', 'User Experience Design'],
@@ -25,7 +25,10 @@ export default {
     image1: waveimage1,
     image2: waveimage2,
     name: 'Wave Accounting',
-    description: 'Description coming soon...',
+    description: `Just snap a photo of your receipts, invoices or bills. Receipts by Wave digitizes (OCR) and imports them directly into your Wave account. Never face a wallet, purse or shoebox full of receipts again. Your finances are always up to date and correct.
+
+My role in the project was as an Agile Engineer, I worked on the iOS app at Xtreme Labs. Xtreme Labs leads the world's biggest brands in creating award-winning mobile products.
+`,
     link: '/portfolio/wave',
     website: 'https://itunes.apple.com/us/app/receipts-by-wave/id619867797',
     roles: ['Agile Engineer', 'iOS'],
@@ -38,7 +41,7 @@ export default {
     image1: sparkgigimage1,
     image2: sparkgigimage2,
     name: 'SparkGig',
-    description: 'Description coming soon...',
+    description: 'SparkGig is an online marketplace to bring live performance into everyday life. SparkGig believes that a product is only as good as the number of performers and event hosts gaining value from it. As a key leader (CTO) of our team I was responsible for web development, product growth, ongoing customer research, product development and business development.',
     link: '/portfolio/sparkgig',
     website: 'http://sparkgig.com',
     roles: ['CTO', 'Software Engineer', 'React + Angular', 'Management', 'User Experience Design'],
@@ -51,7 +54,10 @@ export default {
     image1: kikimage1,
     image2: kikimage2,
     name: 'Kik Bots',
-    description: 'Description coming soon...',
+    description: `First there were websites, then there were apps. Now, there are bots. With chat bots, you can reach consumers in fun, useful, and meaningful ways in an environment that presents the least friction possible. There’s nothing to download, no icons to add to the homescreen, and no memory hogging. And on Kik, your bot could have direct access to more than 300 million registered users.
+    
+    My role at Kik was as a Bot Developer. I worked on the tools that power their chat bots, more specifically the chat engine. There were two engines at the time: the keyword and custom chat engine.
+    `,
     link: '/portfolio/kik',
     website: 'https://kik.com',
     roles: ['Bot Developer', 'Python + Angular', 'Software Engineer'],
