78f402f4dd13e5ed605bce33e983bb49283f1c76
Adrian Carolli <adrian.carolli1@gmail.com>
2017-11-23 13:38:30 -0500

    Meta data specific to page

--- src/components/views/PortfolioPage/index.jsx
+++ src/components/views/PortfolioPage/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Image } from 'semantic-ui-react';
+import { Helmet } from 'react-helmet';
 import { StyledHeroHeader, StyledHero, StyledDescription, StyledButton } from './styles';
 import { SectionContainer, StyledH1, StyledParagraph, StyledHeader } from '../../common';
 
 const PortfolioPage = ({ data }) => (
   <div>
+    <Helmet>
+      <title>Adrian Carolli | {data.name}</title>
+    </Helmet>
     <StyledHeroHeader>
       <StyledHero src={data.image1} />
     </StyledHeroHeader>
